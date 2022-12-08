685c70a3a1c83d0ee3bf616515f202f3afc5d81c
Wesley <84082753+WesleyLuiz21@users.noreply.github.com>
2022-12-08 22:18:41 +0000

    fixes to the design and a broken link in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './navbar.css';
 import emailIcon from './icons/icon-men.png'
+import { NavLink } from 'react-router-dom';
 import pin from './icons/pin.png'
 
 
@@ -18,7 +19,7 @@ function Navbar() {
     return (
         <header className="flex">
             <nav className="flex">
-            <a href="/#" id="myname">{name}</a>
+            <NavLink to="/PortfolioApp" id="myname">{name}</NavLink>
             <ul className="flex" id="right-nav">
                 <li>
                     <img src={emailIcon} class='icons' alt='icon-email' width='39' height='39'></img>
