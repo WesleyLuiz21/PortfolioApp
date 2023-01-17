b28d95ae1299be620be773b951f2cad82dafeec5
unknown <wesley_luiz2001@hotmail.com>
2023-01-17 21:18:17 +0000

    added actual links to the new education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,25 +12,26 @@ const DetailsEdu = () => {
         <div className="detailsEdu">
         <details>
          <summary>Codecademy Courses</summary>
-         <a href="/" className="op-education">Frontend Engineer (in-progress)</a>
+         <p className="op-education">Frontend Engineer (in-progress)</p>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/705dcb15de0da4dd9d9fc4f3274b430e" target='_blank' rel="noreferrer" className="op-education">Javascript</a>
          <br/>
-         <a href="/" className="op-education">Javascript</a>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/0becf7c1cd2bd715f24331dddd23425a" target='_blank' rel='noreferrer' className="op-education">jQuery Course</a>
          <br/>
-         <a href="/" className="op-education">jQuery Course</a>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/0595479d03627a8cb816b069000e4d06" target='_blank' rel='noreferrer' className="op-education">Bootstrap Course</a>
          <br/>
-         <a href="/" className="op-education">Bootstrap Course</a>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/4ccef8d532484ea2aeec3b3b3dbb4f9c" target='_blank' rel='noreferrer' className="op-education">Intro to UI and UX</a>
          <br/>
-         <a href="/" className="op-education">Intro to UI and UX</a>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/0a6884fad1dbf4afe5df084d2ec1e7c3" target='_blank' rel="noreferrer" className="op-education">Color Design</a>
          <br/>
-         <a href="/" className="op-education">Color Design</a>
+         <a href="https://www.codecademy.com/profiles/Wesley_luiz/certificates/3a62023b0054dc793edc0adecd715fd7" target='_blank' rel="noreferrer" className="op-education">Responsive Design</a>
         </details>
         <details>
-        <summary>FreeCodecamp Courses</summary>
+        <summary>FreeCodeCamp Courses</summary>
         <a href="https://www.freecodecamp.org/certification/WesleySouza/responsive-web-design" target="_blank" rel='noreferrer' className="op-education">Responsive Web Design (Aproximately 300 hours)</a>
         </details>
         <details>
         <summary>Code Institute Courses</summary>
-        <a href="/" className="op-education">Intro to Web Development (In-progress)</a>
+        <p className="op-education">Intro to Web Development (In-progress)</p>
         </details>
         <details>
         <summary>Extra</summary>
