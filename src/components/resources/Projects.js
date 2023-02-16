2e1b09e6966f2e63a98bcf4ec9b396a4d4f2521d
Wesley <84082753+WesleyLuiz21@users.noreply.github.com>
2023-02-16 11:36:12 +0000

    added another project to the portfolio component and updated the img links

--- src/components/resources/Projects.js
+++ src/components/resources/Projects.js
@@ -3,17 +3,25 @@ export const projects = [
     {
         title: 'Blogr Landing Page',
         description:  'A landing page challenge provided by Frontend mentor. I have used HTML, CSS3, JavaScript and jQuery.',
-        image: 'https://i.imgur.com/ECkqklO.png',
+        image: 'https://i.imgur.com/jHWyXGL.png',
         link: 'https://wesleyluiz21.github.io/Blogr/',
         github: 'https://github.com/WesleyLuiz21/Blogr',
     },
     {
         title: 'Portfolio Page',
         description:  'My first portfolio page. I have designed it using Figma/Photoshop and build with HTML, CSS3 and JavaScript.',
-        image: 'https://i.imgur.com/1lAQMWz.png',
+        image: 'https://i.imgur.com/UEzWbbv.png',
         link: 'https://wesleyluiz21.github.io/wesleyportifolio/',
         github: 'https://github.com/WesleyLuiz21/wesleyportifolio',
     },
+    {
+        title: 'Quote Generator',
+        description:  'My first project using an API to get quotes and display them nicely. built with HTML, CSS3 and JavaScript.',
+        image: 'https://i.imgur.com/7N4szBT.png',
+        link: 'https://wesleyluiz21.github.io/Quote-Generator/',
+        github: 'https://github.com/WesleyLuiz21/Quote-Generator',
+    },
+    
     
 ];
 export default projects;
